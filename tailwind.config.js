722d979340fcc7da409fea63e0004780dee5c002
Zach Nelson <hello@zacharynelson.me>
2025-06-08 18:14:20 -0500

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,26 @@ module.exports = {
     require('flowbite-typography'),
   ],
   theme: {
-      extend: {
-          colors: {
-              primary: {
-                  50: '#eff6ff',
-                  100: '#dbeafe',
-                  200: '#bfdbfe',
-                  300: '#93c5fd',
-                  400: '#60a5fa',
-                  500: '#3b82f6',
-                  600: '#2563eb',
-                  700: '#1d4ed8',
-                  800: '#1e40af',
-                  900: '#1e3a8a',
-              }
-          }
+    extend: {
+      colors: {
+        'primary': {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          500: '#3b82f6',
+          600: '#2563eb',
+          900: '#1e3a8a'
+        },
+        'accent-cyan': {
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2'
+        },
+        'accent-coral': {
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48'
+        }
       }
+    }
   },
 }
